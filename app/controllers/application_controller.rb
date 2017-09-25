b7c45330851fa20640b5583b8efc81b1852687da
Kevin C McKenna <kevmckenna@gmail.com>
2017-09-25 12:33:26 -0700

    Initial Commit added bootstrap and bcrypt

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,22 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  helper_method :current_user, :logged_in?, :authorize
+  # I want current user and logged in available in templates for other pages and views
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    # return current user or set it to what is found in cookie, only if there is one to begin with
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def authorize
+    unless logged_in?
+      flash[:danger] = "You must be logged in to view that."
+      redirect_to new_session_path
+    end
+  end
+  # unless session ID the user ID of the "thing", then deny.... then add it to helper
+end
---
+++ db/migrate/20170925183034_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+      t.string :password_confirmation
+      t.string :location
+      t.integer :zip_code
+      t.text :bio
+
+      t.timestamps
+    end
+  end
+end
