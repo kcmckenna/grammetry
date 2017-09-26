4e6258219caec6fbce73275308e52c65a54ea3ab
Kevin C McKenna <kevmckenna@gmail.com>
2017-09-26 11:25:51 -0700

    minor updates, fixing broken post links

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,10 +15,12 @@ class PostsController < ApplicationController
 
   def create
     @post = current_user.posts.new(post_params)
+    # adds a new post to the user's post list
     if @post.save
       redirect_to post_path(@post)
     else
       redirect_to new_post_path
+    end
   end
 
   def edit
@@ -35,4 +37,5 @@ class PostsController < ApplicationController
   def post_params
     params.require(:post).permit(:title, :body, :pic_url)
   end
+
 end
