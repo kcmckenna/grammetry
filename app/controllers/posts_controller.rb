2848e411e75bdbf93347acd5edde2bf4cb052dec
Kevin C McKenna <kevmckenna@gmail.com>
2017-09-26 08:58:34 -0700

    added post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,38 @@
 class PostsController < ApplicationController
+  before_action :authorize, except: [:index, :show]
+
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      redirect_to new_post_path
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body, :pic_url)
   end
 end
